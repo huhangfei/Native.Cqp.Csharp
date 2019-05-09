0a8215f1ff9a3b61ca56b6f8a1ae3ba051646b6e
huhangfei <huhangfei@yiche.com>
2019-05-09 12:09:27 +0800

    手动发送功能

--- Native.Csharp/Business/UI/Main.Designer.cs
+++ Native.Csharp/Business/UI/Main.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sendRecordBtn = new System.Windows.Forms.Button();
+            this.groupIdText = new System.Windows.Forms.TextBox();
+            this.recordPathText = new System.Windows.Forms.TextBox();
+            this.sendTxtBtn = new System.Windows.Forms.Button();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendRecordBtn
+            // 
+            this.sendRecordBtn.Location = new System.Drawing.Point(431, 24);
+            this.sendRecordBtn.Name = "sendRecordBtn";
+            this.sendRecordBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendRecordBtn.TabIndex = 0;
+            this.sendRecordBtn.Text = "发送";
+            this.sendRecordBtn.UseVisualStyleBackColor = true;
+            this.sendRecordBtn.Click += new System.EventHandler(this.sendRecordBtn_Click);
+            // 
+            // groupIdText
+            // 
+            this.groupIdText.Location = new System.Drawing.Point(25, 26);
+            this.groupIdText.Name = "groupIdText";
+            this.groupIdText.Size = new System.Drawing.Size(100, 21);
+            this.groupIdText.TabIndex = 1;
+            this.groupIdText.Text = "852797927";
+            // 
+            // recordPathText
+            // 
+            this.recordPathText.Location = new System.Drawing.Point(165, 26);
+            this.recordPathText.Name = "recordPathText";
+            this.recordPathText.Size = new System.Drawing.Size(239, 21);
+            this.recordPathText.TabIndex = 2;
+            this.recordPathText.Text = "404.mp3";
+            // 
+            // sendTxtBtn
+            // 
+            this.sendTxtBtn.Location = new System.Drawing.Point(431, 127);
+            this.sendTxtBtn.Name = "sendTxtBtn";
+            this.sendTxtBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendTxtBtn.TabIndex = 3;
+            this.sendTxtBtn.Text = "发送";
+            this.sendTxtBtn.UseVisualStyleBackColor = true;
+            this.sendTxtBtn.Click += new System.EventHandler(this.sendTxtBtn_Click);
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(165, 54);
+            this.messageText.Multiline = true;
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(239, 96);
+            this.messageText.TabIndex = 4;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.sendTxtBtn);
+            this.Controls.Add(this.recordPathText);
+            this.Controls.Add(this.groupIdText);
+            this.Controls.Add(this.sendRecordBtn);
+            this.Name = "Main";
             this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sendRecordBtn;
+        private System.Windows.Forms.TextBox groupIdText;
+        private System.Windows.Forms.TextBox recordPathText;
+        private System.Windows.Forms.Button sendTxtBtn;
+        private System.Windows.Forms.TextBox messageText;
     }
 }
--- Native.Csharp/Business/UI/Main.cs
+++ Native.Csharp/Business/UI/Main.cs
@@ -15,5 +15,40 @@ namespace Native.Csharp.App.UI
         {
             InitializeComponent();
         }
+
+        private void sendRecordBtn_Click(object sender, EventArgs e)
+        {
+            string path = recordPathText.Text;
+            SendAllGroup(Common.CqApi.CqCode_Record(path));
+        }
+
+        private void sendTxtBtn_Click(object sender, EventArgs e)
+        {
+            string msg = messageText.Text;
+            SendAllGroup(msg);
+        }
+
+        private int[] GetAllId()
+        {
+            string ids = groupIdText.Text;
+
+            return ids.Split(',').Select(x=>int.Parse(x)).ToArray();
+        }
+
+        private void SendAllGroup(string msg)
+        {
+            try
+            {
+                int[] ids = GetAllId();
+                foreach (int id in ids)
+                {
+                    Common.CqApi.SendGroupMessage(id, msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
